604969b5fd7fb192c4f4e167c752bae0f7f283ba
Milad Khajavi <khajavi@gmail.com>
2022-10-27 10:03:52 +0330

    cleanup.

--- zio-sbt-website/src/main/scala/zio/sbt/WebsitePlugin.scala
+++ zio-sbt-website/src/main/scala/zio/sbt/WebsitePlugin.scala
@@ -2,45 +2,40 @@ package zio.sbt
 
 import mdoc.MdocPlugin
 import mdoc.MdocPlugin.autoImport.*
-import sbt.{Def, *}
+import sbt.*
+import sbt.Keys.*
 
 import java.nio.file.Paths
+import scala.language.postfixOps
 
 object WebsitePlugin extends sbt.AutoPlugin {
 
   object autoImport {
-    val projectName = settingKey[String]("project name")
-    val installWebsiteTheme = taskKey[Unit]("install website theme")
     val compileDocs = taskKey[Unit]("compile docs")
     val installWebsite = taskKey[Unit]("install the website for the first time")
-    val docusaurusServer = taskKey[Unit]("run docusaurus")
     val previewWebsite = taskKey[Unit]("preview website")
   }
 
   import autoImport.*
 
-  override def requires =
-    MdocPlugin
+  override def requires = MdocPlugin
 
   override lazy val projectSettings =
     Seq(
-      installWebsiteTheme := installWebsiteThemeTask.value,
       compileDocs := compileDocsTask.value,
       mdocOut := Paths.get("website/docs").toFile,
-      installWebsite := Def.sequential(installWebsiteTheme, compileDocs).value,
-      docusaurusServer := docusaurusServerTask.value,
+      installWebsite := installWebsiteTask.value,
       previewWebsite := previewWebsiteTask.value
     )
 
   lazy val previewWebsiteTask = Def.task {
     import zio.*
 
-    val task = ZIO.scoped {
+    val task =
       for {
-        _ <- ZIO.attempt(compileDocsTask.value).debug.forkScoped
+        _ <- ZIO.attempt(compileDocsTask.value).forkDaemon
         _ <- ZIO.attempt(docusaurusServerTask.value)
       } yield ()
-    }
 
     Unsafe.unsafe { implicit unsafe =>
       Runtime.default.unsafe.run(task).getOrThrowFiberFailure()
@@ -58,21 +53,22 @@ object WebsitePlugin extends sbt.AutoPlugin {
       mdoc.toTask(" --watch --no-livereload")
     }
 
-  lazy val installWebsiteThemeTask =
+  lazy val installWebsiteTask =
     Def.task {
       import sys.process.*
 
       val task =
-        s"""|npx @zio.dev/create-zio-website@latest website \\
-            |  --description="Documentation of folan" \\
-            |  --author="Milad Khajavi" \\
-            |  --email="khajavi@gmail.com" \\
+        s"""|npx @zio.dev/create-zio-website@latest ${normalizedName.value} \\
+            |  --description="${name.value}" \\
+            |  --author="ZIO Contributors" \\
+            |  --email="email@zio.dev" \\
             |  --license="Apache-2.0" \\
             |  --architecture=Linux""".stripMargin
 
-      println(s"executing the following task: \n$task")
-
+      println(s"installing website for ${normalizedName.value} ... \n$task")
       task !
+
+      s"mv ${normalizedName.value} website" !
 
       "rm website/.git/ -rvf" !
     }
