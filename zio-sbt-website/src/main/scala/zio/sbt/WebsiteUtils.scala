c970cbcab46c7d8745bde32be96857bd25d12bf0
Milad Khajavi <khajavi@gmail.com>
2023-02-06 13:23:47 +0330

    Add Release Workflow (#121)
    * remove docs project from testing.
    
    * generate other workflows.
    
    * rename.
    
    * refactor.
    
    * update site workflow.

--- zio-sbt-website/src/main/scala/zio/sbt/WebsiteUtils.scala
+++ zio-sbt-website/src/main/scala/zio/sbt/WebsiteUtils.scala
@@ -222,6 +222,18 @@ object WebsiteUtils {
         )
       )
 
+      val Release: Step.SingleStep =
+        Step.SingleStep(
+          name = "Release",
+          run = Some("sbt ci-release"),
+          env = Map(
+            "PGP_PASSPHRASE"    -> "${{ secrets.PGP_PASSPHRASE }}",
+            "PGP_SECRET"        -> "${{ secrets.PGP_SECRET }}",
+            "SONATYPE_PASSWORD" -> "${{ secrets.SONATYPE_PASSWORD }}",
+            "SONATYPE_USERNAME" -> "${{ secrets.SONATYPE_USERNAME }}"
+          )
+        )
+
       val Lint: Step.SingleStep = Step.SingleStep(
         name = "Lint",
         run = Some("sbt lint")
@@ -270,6 +282,13 @@ object WebsiteUtils {
       .pretty(
         Workflow(
           name = "Website",
+          env = Map(
+            // JDK_JAVA_OPTIONS is _the_ env. variable to use for modern Java
+            "JDK_JAVA_OPTIONS" -> "-XX:+PrintCommandLineFlags -Xmx6G -Xss4M -XX:+UseG1GC",
+            // For Java 8 only (sadly, it is not modern enough for JDK_JAVA_OPTIONS)
+            "JVM_OPTS"     -> "-XX:+PrintCommandLineFlags -Xmx6G -Xss4M -XX:+UseG1GC",
+            "NODE_OPTIONS" -> "--max_old_space_size=6144"
+          ),
           triggers = Seq(
             Trigger.WorkflowDispatch(),
             Trigger.Release(Seq("published")),
@@ -335,8 +354,24 @@ object WebsiteUtils {
               )
             ),
             Job(
+              id = "release",
+              name = "Release",
+              need = Seq("build", "lint", "test"),
+              condition = Some(
+                Condition.Expression("github.event_name != 'pull_request'") &&
+                  (Condition.Expression("github.ref == 'refs/heads/main'") ||
+                    Condition.Expression("startsWith(github.ref, 'refs/tags/v')"))
+              ),
+              steps = Seq(
+                Checkout,
+                SetupJava(),
+                Release
+              )
+            ),
+            Job(
               id = "publish-docs",
               name = "Publish Docs",
+              need = Seq("release"),
               condition = Some(
                 Condition.Expression("github.event_name == 'release'") &&
                   Condition.Expression("github.event.action == 'published'") || Condition.Expression(
