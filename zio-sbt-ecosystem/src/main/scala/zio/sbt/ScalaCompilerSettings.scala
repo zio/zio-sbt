080b598b2264ad95358bc2c6e058ed5cee7967ac
Milad Khajavi <khajavi@gmail.com>
2023-02-28 15:56:29 +0330

    remove scala3 from scala3Settings.

--- zio-sbt-ecosystem/src/main/scala/zio/sbt/ScalaCompilerSettings.scala
+++ zio-sbt-ecosystem/src/main/scala/zio/sbt/ScalaCompilerSettings.scala
@@ -54,7 +54,6 @@ trait ScalaCompilerSettings {
     else Nil
 
   lazy val scala3Settings: Seq[Setting[_]] = Seq(
-    crossScalaVersions += ZioSbtEcosystemPlugin.autoImport.scala3.value,
     scalacOptions ++= {
       if (Keys.scalaBinaryVersion.value == "3")
         Seq("-noindent")
