e7ddcd180f7903783f73bc144fa76b4bfdd57edc
jules Ivanic <jules.ivanic@gmail.com>
2024-03-18 16:12:27 +0800

    Update actions

--- zio-sbt-ci/src/main/scala/zio/sbt/V.scala
+++ zio-sbt-ci/src/main/scala/zio/sbt/V.scala
@@ -3,12 +3,12 @@ package zio.sbt
 object V {
   def apply(packageName: String): String =
     Map(
-      "peter-evans/create-pull-request" -> "v6.0.1",
+      "peter-evans/create-pull-request" -> "v6.0.2",
       "zio/generate-github-app-token"   -> "v1.0.0",
       "pierotofy/set-swap-space"        -> "master",
       "actions/checkout"                -> "v4.1.1",
       "coursier/cache-action"           -> "v6.4.5",
-      "actions/setup-java"              -> "v4.1.0",
+      "actions/setup-java"              -> "v4.2.1",
       "actions/setup-node"              -> "v4"
     ).map { case (k, v) => (k, s"$k@$v") }.apply(packageName)
 }
