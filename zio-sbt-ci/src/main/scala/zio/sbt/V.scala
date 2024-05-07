7cc74fd905822b1f9fdef6e837bceab679bc43fa
jules Ivanic <jules.ivanic@gmail.com>
2024-05-07 23:20:58 +0800

    Only use the major version of each Github Actions to avoid to have to update them constantly

--- zio-sbt-ci/src/main/scala/zio/sbt/V.scala
+++ zio-sbt-ci/src/main/scala/zio/sbt/V.scala
@@ -3,12 +3,12 @@ package zio.sbt
 object V {
   def apply(packageName: String): String =
     Map(
-      "peter-evans/create-pull-request" -> "v6.0.3",
+      "peter-evans/create-pull-request" -> "v6",
       "zio/generate-github-app-token"   -> "v1.0.0",
       "pierotofy/set-swap-space"        -> "master",
-      "actions/checkout"                -> "v4.1.2",
-      "coursier/cache-action"           -> "v6.4.6",
-      "actions/setup-java"              -> "v4.2.1",
+      "actions/checkout"                -> "v4",
+      "coursier/cache-action"           -> "v6",
+      "actions/setup-java"              -> "v4",
       "actions/setup-node"              -> "v4"
     ).map { case (k, v) => (k, s"$k@$v") }.apply(packageName)
 }
