426da9dc213d239f3125ab4b5b12616d4abc1327
Thijs Broersen <thijsbroersen@gmail.com>
2024-11-27 21:00:17 +0100

    updates and dedup scalaworkflow

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 // Build Server Plugins
-addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "2.0.3")
+addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "2.0.5")
 
 // Linting Plugins
 addSbtPlugin("org.scalameta"    % "sbt-scalafmt"              % "2.5.4")
--- zio-sbt-ci/src/main/scala/zio/sbt/V.scala
+++ zio-sbt-ci/src/main/scala/zio/sbt/V.scala
@@ -5,7 +5,7 @@ object V {
     Map(
       "peter-evans/create-pull-request" -> "v7",
       "zio/generate-github-app-token"   -> "v1.0.0",
-      "pierotofy/set-swap-space"        -> "master",
+      "pierotofy/set-swap-space"        -> "49819abfb41bd9b44fb781159c033dba90353a7c", // 1.0,
       "actions/checkout"                -> "v4",
       "coursier/cache-action"           -> "v6",
       "actions/setup-java"              -> "v4",
