e7ca74221dd719e5bf65273a7ad777d7573f14ae
Jules Ivanic <jules.ivanic@gmail.com>
2025-09-23 10:38:32 +0400

    scalafix

--- zio-sbt-ecosystem/src/main/scala/zio/sbt/ZioSbtEcosystemPlugin.scala
+++ zio-sbt-ecosystem/src/main/scala/zio/sbt/ZioSbtEcosystemPlugin.scala
@@ -20,11 +20,11 @@ import scala.collection.immutable.ListMap
 
 import com.jsuereth.sbtpgp.SbtPgp.autoImport._
 import org.scalafmt.sbt.ScalafmtPlugin
-import sbtheader.HeaderPlugin
 import sbt.Keys._
 import sbt.nio.Keys.{ReloadOnSourceChanges, onChangedBuildSource}
 import sbt.{Def, _}
 import sbtbuildinfo.BuildInfoPlugin
+import sbtheader.HeaderPlugin
 import scalafix.sbt.ScalafixPlugin
 
 object ZioSbtEcosystemPlugin extends AutoPlugin {
